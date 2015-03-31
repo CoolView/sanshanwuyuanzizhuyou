a716af1202f998932271fe8e2a06d61a86e0012d
CoolView <284226000@qq.com>
2015-03-31 17:24:54 +0800

    修复点击用浏览器打开不是网址报错 错误

--- src/com/bin/sanshanwuyuanlvyou/zhinanzhen/CaptureResultActivity.java
+++ src/com/bin/sanshanwuyuanlvyou/zhinanzhen/CaptureResultActivity.java
@@ -1,6 +1,5 @@
 package com.bin.sanshanwuyuanlvyou.zhinanzhen;
 
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -10,6 +9,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bin.sanshanwuyuanlvyou.R;
 import com.google.zxing.client.android.Intents;
@@ -18,23 +18,23 @@ public class CaptureResultActivity extends Activity {
 	private TextView tvResult;
 	private Button btOpenAddress;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_capture_result);
-        tvResult = (TextView) findViewById(R.id.tv_result);
-        btOpenAddress = (Button) findViewById(R.id.bt_open_address);
-        initData();
-    }
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_capture_result);
+		tvResult = (TextView) findViewById(R.id.tv_result);
+		btOpenAddress = (Button) findViewById(R.id.bt_open_address);
+		initData();
+	}
 
-    private void initData() {
+	private void initData() {
 
-        final Intent intent = getIntent();
-        if (null != intent) {
-            tvResult.setText(intent.getStringExtra(Intents.Scan.RESULT));
-            btOpenAddress.setOnClickListener(new OnClickListener() {
-				
+		final Intent intent = getIntent();
+		if (null != intent) {
+			tvResult.setText(intent.getStringExtra(Intents.Scan.RESULT));
+			btOpenAddress.setOnClickListener(new OnClickListener() {
+
 				@Override
 				public void onClick(View arg0) {
 					// TODO Auto-generated method stub
@@ -42,12 +42,19 @@ public class CaptureResultActivity extends Activity {
 					Log.d("hahhahhaha", address);
 					// 更多隐式意图用法
 					// 调用浏览器打开网页
-					Intent intent2 = new Intent(Intent.ACTION_VIEW,Uri.parse(address));
-//					intent2.setData(Uri.parse("address"));
-					startActivity(intent2);
+					if (address != null)
+						if (address.startsWith("http://")) {
+							Intent intent2 = new Intent(Intent.ACTION_VIEW, Uri
+									.parse(address));
+							// intent2.setData(Uri.parse("address"));
+							startActivity(intent2);
+						}else {
+							Toast.makeText(getApplicationContext(), "网址才可以使用浏览器打开", Toast.LENGTH_SHORT).show();
+						}
+
 				}
 			});
-            
-        }
-    }
+
+		}
+	}
 }
