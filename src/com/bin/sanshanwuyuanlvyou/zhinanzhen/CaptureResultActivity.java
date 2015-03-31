7a408d46274c167e003b19310cfd4dfb5237439f
CoolView <284226000@qq.com>
2015-03-31 17:11:30 +0800

    二维码完成

--- src/com/bin/sanshanwuyuanlvyou/zhinanzhen/CaptureResultActivity.java
+++ src/com/bin/sanshanwuyuanlvyou/zhinanzhen/CaptureResultActivity.java
@@ -3,7 +3,12 @@ package com.bin.sanshanwuyuanlvyou.zhinanzhen;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.bin.sanshanwuyuanlvyou.R;
@@ -11,32 +16,38 @@ import com.google.zxing.client.android.Intents;
 
 public class CaptureResultActivity extends Activity {
 	private TextView tvResult;
-    private TextView tvResultFormat;
-    private TextView tvUri;
+	private Button btOpenAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capture_result);
-        initActivity();
-        initData();
-    }
-
-    private void initActivity() {
-
         tvResult = (TextView) findViewById(R.id.tv_result);
-        tvResultFormat = (TextView) findViewById(R.id.tv_result_format);
-        tvUri = (TextView) findViewById(R.id.tv_uri);
+        btOpenAddress = (Button) findViewById(R.id.bt_open_address);
+        initData();
     }
 
     private void initData() {
 
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         if (null != intent) {
             tvResult.setText(intent.getStringExtra(Intents.Scan.RESULT));
-            tvResultFormat.setText(intent.getStringExtra(Intents.Scan.RESULT_FORMAT));
-            tvUri.setText(intent.toUri(intent.getFlags()));
+            btOpenAddress.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					// TODO Auto-generated method stub
+					String address = intent.getStringExtra(Intents.Scan.RESULT);
+					Log.d("hahhahhaha", address);
+					// 更多隐式意图用法
+					// 调用浏览器打开网页
+					Intent intent2 = new Intent(Intent.ACTION_VIEW,Uri.parse(address));
+//					intent2.setData(Uri.parse("address"));
+					startActivity(intent2);
+				}
+			});
+            
         }
     }
 }
